12b6e2b8b77530057b656c080791ee2c72456fbe
muhammad ali <muhammad.alfaqoui@gmail.com>
2023-01-10 00:53:30 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:weather_app/screen/home.dart';
+
+void main(){
+  runApp( const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      title: 'Weather ',
+    );
+  }
+}
